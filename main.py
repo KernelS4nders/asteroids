cfd85764c524dfd182111c8fe78a7ebff0b7ca6a
kernels4nders <jb.markowicz2014@protonmail.com>
2025-11-02 15:57:14 -0500

    2:Optimized for 60fps

--- main.py
+++ main.py
@@ -6,17 +6,29 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
-    # setting screen dimensions for pygame
+    # setting screen variable and its dimensions for pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # While Loop
     running = True
+    
+    # FPS
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while running:
+        # This allows for the close button to work in the upper left hand corner
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill(color=(0,0,0), rect=None, special_flags=0)
-            pygame.display.flip()
+        # Fills screen variable with black color
+        screen.fill(color=(0,0,0))
+        # This allows for smooth screen refresh - allows for background and foreground data streams to swap - flicker gone
+        pygame.display.flip()
+        # pygame.time.Clock.tick(60)
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
 
 
 
